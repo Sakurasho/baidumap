aee7350836bf9e262699a568b3c430028ab82787
ShaYoung <31478978+Sakurasho@users.noreply.github.com>
2020-11-20 15:56:35 +0800

    Add files via upload

--- R/getRoute.R
+++ R/getRoute.R
@@ -1,80 +1,97 @@
-#' Get route from query
-#' Take in a original location and destination, return the direction
-#' @param origin the original location
-#' @param destination the destination
-#' @param mode 'driving'(default), 'walking', or 'transit'
-#' @param region the city of of original location and destination. 
-#' If original and destination is not in the same city, 
-#' set `origin_region` and `destination_region` seperately.
-#' @param origin_region the city of original location. If not set, use `region` instead.
-#' @param destination_region the city of destination. If not set, use `region` instead.
-#' @param tactics 10(no expressway), 11(default, shortest time), 12(shortest path).
-#' @param coord_type 'bd09ll'(default), 'gcj02'(which Google map and Soso map are using), 'wgs84' for GPS devices.
-#' @return a data frame contains longtitude and latitude of the route.
-#' @export getRoute
-#' @importFrom RCurl getForm
-#' @importFrom XML htmlTreeParse　xpathSApply xmlValue
-#' 
-#' @examples
-#' \dontrun{
-#' bjMap = getBaiduMap('北京', color='bw')
-#' df = getRoute('首都国际机场', '北京南苑机场', region = '北京')
-#' ggmap(bjMap) + geom_path(data = df, aes(lon, lat), alpha = 0.5, col = 'red')
-#' }
-#' 
 
+
+
+#=============================================================================
 getRoute = function(...){
-    rawData = getRouteXML(...)
-    return(xml2df(rawData))
+  rawData = getRouteXML(...)
+  return(xml2df(rawData))
 }
 
 getRouteXML = function(origin, destination, mode='driving', 
-                       region = '北京', origin_region = NA, 
+                       region = '上海', origin_region = NA, 
                        destination_region = NA, 
-                       tactics = 11, 
+                       tactics = 0, 
                        coord_type = 'bd09ll',
+                       ret_coordtype = 'bd09ll',
                        output = 'xml',
                        map_ak=''){
-    if (map_ak == '' && is.null(getOption('baidumap.key'))){
-        stop(Notification)
-    }else{
-        map_ak = ifelse(map_ak == '', getOption('baidumap.key'), map_ak)
+  
+  if (map_ak == '' && is.null(getOption('baidumap.key'))){
+    stop("Notification")
+  }else{
+    map_ak = ifelse(map_ak == '', getOption('baidumap.key'), map_ak)
+  }
+  
+  
+  if (is.na(region)){
+    if (is.na(origin_region) & is.na(destination_region)) {
+      stop('Argument "region" is not setted!')
     }
-    if (is.na(region)){
-        if (is.na(origin_region) & is.na(destination_region)) {
-            stop('Argument "region" is not setted!')
-        }
-    }
-    get_city = function(x) ifelse(is.na(x), region, x)
-    origin_region = get_city(origin_region)
-    destination_region = get_city(destination_region)
-    ## get xml data
-    serverAddress = 'http://api.map.baidu.com/direction/v1'
-    rawData = getForm(serverAddress, mode = mode, 
-                      origin = origin, destination = destination, 
-                      origin_region = origin_region, 
-                      destination_region = destination_region,
-                      tactics = tactics, coord_type = coord_type, 
-                      ak = map_ak, output = output)
-    return(rawData)
+  }
+  
+  get_city = function(x) ifelse(is.na(x), region, x)
+  
+  origin_region = get_city(origin_region)
+  destination_region = get_city(destination_region)
+  ## get xml data
+  serverAddress = 'http://api.map.baidu.com/directionlite/v1/driving?'
+  rawData = getForm(serverAddress, 
+                    origin = "31.216326,121.479177", destination = "31.238521,121.482195", 
+                    # origin_region = origin_region, 
+                    # destination_region = destination_region,
+                    #tactics = tactics, coord_type = coord_type, ret_coordtype = ret_coordtype,
+                    ak = map_ak, output = output)
+  return(rawData)
 }
 
-xml2df = function(rawData){
-    ## extract longitude and latitude
-    tree = htmlTreeParse(rawData, useInternal = TRUE)
-    path = xpathSApply(tree, "//path",  xmlValue)
-    split_path = function(x){
-        xVec = strsplit(x, ';')[[1]]
-        xMat = sapply(xVec, function(x) as.numeric(strsplit(x, ',')[[1]]))
-        xDf = data.frame(t(xMat), row.names = NULL)
-        colnames(xDf) = c('lon', 'lat')
-        return(xDf)
-    }
-    coor_list = lapply(path, split_path)
-    ## return a dataframe
-    coors = do.call(rbind, coor_list)
-    return(coors)
+## Json文件解析
+Json2df = function(rawData){
+  temp <- rjson::fromJSON(rawData)
+  
+}
+
+library("rjson")
+
+# library(jsonlite)
+temp <- rjson::fromJSON(rawData)
+# temp1 <- jsonlite::fromJSON(rawData, simplifyMatrix = TRUE)
+
+summary(temp)
+temp$status # 状态码，0：成功，1：服务内部错误，2：参数无效，7：无返回结果
+temp$message # 状态码对应的信息
+temp$result$origin$lng
+temp$result$origin$lat
+
+
+
+Step_Num <- length(temp$result$route[[1]]$steps) # 计算step的总数
+df.all <- data.frame()
+for (N in 1:Step_Num){
+  pathLine <- temp$result$route[[1]]$steps[[N]]$path
+  df <- data.frame(matrix(unlist(strsplit(unlist(strsplit(pathLine, ";")), ",")), ncol=2, byrow = TRUE))
+  colnames(df)<- c("Lon", "Lat")
+  df.all <- rbind(df.all, df)
+  df.all$Lon <- as.numeric(df.all$Lon)
+  df.all$Lat <- as.numeric(df.all$Lat)
 }
 
 
+
+# xml2df = function(rawData){
+#   ## extract longitude and latitude
+#   tree = htmlTreeParse(rawData, useInternal = TRUE)
+#   path = xpathSApply(tree, "//path",  xmlValue)
+#   split_path = function(x){
+#     xVec = strsplit(x, ';')[[1]]
+#     xMat = sapply(xVec, function(x) as.numeric(strsplit(x, ',')[[1]]))
+#     xDf = data.frame(t(xMat), row.names = NULL)
+#     colnames(xDf) = c('lon', 'lat')
+#     return(xDf)
+#   }
+#   
+#   coor_list = lapply(path, split_path)
+#   ## return a dataframe
+#   coors = do.call(rbind, coor_list)
+#   return(coors)
+# }
 
